4faee7a14a1397ebe0dca5e239ffb3dbbeb7cde6
snow.y <snow.y@kakaopaycorp.com>
2021-07-13 01:06:52 +0900

    Add spring start project

---
+++ src/main/java/com/developers/dmaker/DmakerApplication.java
@@ -0,0 +1,13 @@
+package com.developers.dmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/developers/dmaker/DmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developers.dmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
