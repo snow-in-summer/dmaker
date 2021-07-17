92f4cd54c10279f13b7b7c259995c120411210be
snow-in-summer <seyol@kakao.com>
2021-07-17 18:19:43 +0900

    Add Controller tests

---
+++ src/test/java/com/developers/dmaker/controller/DMakerControllerTest.java
@@ -0,0 +1,77 @@
+package com.developers.dmaker.controller;
+
+import com.developers.dmaker.dto.DeveloperDto;
+import com.developers.dmaker.service.DMakerService;
+import com.developers.dmaker.type.DeveloperLevel;
+import com.developers.dmaker.type.DeveloperSkillType;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author Snow
+ */
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(DMakerController.class)
+@MockBean(JpaMetamodelMappingContext.class)
+class DMakerControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private DMakerService dMakerService;
+
+    protected MediaType contentType =
+            new MediaType(MediaType.APPLICATION_JSON.getType(),
+                    MediaType.APPLICATION_JSON.getSubtype(),
+                    StandardCharsets.UTF_8);
+
+    @Test
+    void getAllDevelopers() throws Exception {
+        //given
+        DeveloperDto seniorBEDeveloper = DeveloperDto.builder()
+                .developerSkillType(DeveloperSkillType.BACK_END)
+                .developerLevel(DeveloperLevel.SENIOR)
+                .memberId("memberId")
+                .build();
+        DeveloperDto juniorFEDeveloper = DeveloperDto.builder()
+                .developerSkillType(DeveloperSkillType.FRONT_END)
+                .developerLevel(DeveloperLevel.JUNIOR)
+                .memberId("memberId2")
+                .build();
+        given(dMakerService.getAllEmployedDevelopers())
+                .willReturn(Arrays.asList(seniorBEDeveloper, juniorFEDeveloper));
+
+        //when
+        //then
+        mockMvc.perform(get("/developers").contentType(contentType))
+                .andExpect(status().isOk())
+                .andExpect(
+                        jsonPath("$.[0].developerSkillType",
+                                is(DeveloperSkillType.BACK_END.name())))
+                .andExpect(
+                        jsonPath("$.[0].developerLevel",
+                                is(DeveloperLevel.SENIOR.name())))
+                .andExpect(
+                        jsonPath("$.[1].developerSkillType",
+                                is(DeveloperSkillType.FRONT_END.name())))
+                .andExpect(
+                        jsonPath("$.[1].developerLevel",
+                                is(DeveloperLevel.JUNIOR.name())));
+    }
+}
