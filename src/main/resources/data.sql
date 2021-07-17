7823550d2a6a8c2ec148108c38d724e4c7c6eb1d
snow-in-summer <seyol@kakao.com>
2021-07-17 15:17:49 +0900

    Add schema and data sql files

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into developer (age, developer_level, developer_skill_type, experience_years, member_id, name, created_at, updated_at)
+VALUES (41, 'SENIOR', 'FULL_STACK', 14, 'snow.y', 'seyol', now(), now());
+
+insert into developer (age, developer_level, developer_skill_type, experience_years, member_id, name, created_at, updated_at)
+VALUES (36, 'JUNIOR', 'FRONT_END', 2, 'sunny.flower', 'sun', now(), now());
