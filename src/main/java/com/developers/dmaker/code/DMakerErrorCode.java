67fb3c3cbc5f973cac2dc54e18fbfbb29bd96bbc
snow-in-summer <seyol@kakao.com>
2021-07-17 22:49:53 +0900

    Add new code

--- src/main/java/com/developers/dmaker/code/DMakerErrorCode.java
+++ src/main/java/com/developers/dmaker/code/DMakerErrorCode.java
@@ -10,6 +10,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum DMakerErrorCode {
     NO_DEVELOPER("해당되는 개발자가 없습니다."),
+    INTERNAL_SERVER_ERROR("서버에 오류가 발생했습니다."),
     INVALID_REQUEST("잘못된 요청입니다.");
 
     private final String message;
